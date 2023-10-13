3dcf7484966dc1a12bae81f139a87c7cddd01f87
Likhith N <likhith.gowda122200@gmail.com>
2023-10-13 12:05:41 +0530

    Book Store Project

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://likhithgowda122200:Naruto722@cluster0.t80inhc.mongodb.net/?retryWrites=true&w=majorityty";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
